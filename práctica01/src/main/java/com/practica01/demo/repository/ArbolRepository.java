900d6bb32784a9901587eb4b6d079562d2a08901
Samuel Sandoval <ssandoval40716@ufide.ac.cr>
2025-10-17 11:28:54 -0600

    Comentario Prueba Branch

--- práctica01/src/main/java/com/practica01/demo/repository/ArbolRepository.java
+++ práctica01/src/main/java/com/practica01/demo/repository/ArbolRepository.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+//Comentario Prueba de Branch
 
 package com.practica01.demo.repository;
 import com.practica01.model.Arbol;
