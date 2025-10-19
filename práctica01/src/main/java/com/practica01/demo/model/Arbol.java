aef1cc52c21f854a9b54dc464a94c177a5bb2315
Adbeel99 <103293313+Adbeel99@users.noreply.github.com>
2025-10-19 16:46:57 -0600

    Avance

--- práctica01/src/main/java/com/practica01/demo/model/Arbol.java
+++ práctica01/src/main/java/com/practica01/demo/model/Arbol.java
@@ -4,28 +4,39 @@
  */
 
 package com.practica01.demo.model;
+ 
 import jakarta.persistence.*;
 import lombok.Data;
-
+ 
 @Entity
 @Data
 @Table(name = "arbol")
-
 public class Arbol {
  
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_arbol;
+    @Column(name = "id_arbol")
+    private Long idArbol;
  
-    private String nombre_comun;
-    private String tipo_flor;
-    private String dureza_madera;
-    private Double altura_promedio;
-    private Integer edad_promedio;
-    private String ruta_imagen;
+    @Column(name = "nombre_comun", nullable = false, length = 100)
+    private String nombreComun;
+ 
+    @Column(name = "tipo_flor", length = 100)
+    private String tipoFlor;
+ 
+    @Column(name = "dureza_madera", length = 50)
+    private String durezaMadera;
+ 
+    @Column(name = "altura_promedio")
+    private Double alturaPromedio;
+ 
+    @Column(name = "edad_promedio")
+    private Integer edadPromedio;
+ 
+    @Column(name = "ruta_imagen")
+    private String rutaImagen;
  
     @ManyToOne
-    @JoinColumn(name = "id_estado")
+    @JoinColumn(name = "id_estado", nullable = false)
     private Estado estado;
 }
-
