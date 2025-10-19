70bb988aabe64758251491ea1f16c1da66be9023
Alejandro <alejandromonteror@gmail.com>
2025-10-19 16:49:05 -0600

    Segundo Commit
    Commit Medio Funcional error 404

--- práctica01/src/main/java/com/practica01/demo/model/Estado.java
+++ práctica01/src/main/java/com/practica01/demo/model/Estado.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.practica01.demo.model;
+
 import jakarta.persistence.*;
 import lombok.Data;
- 
+import java.util.List;
+
 @Entity
 @Data
 @Table(name = "estado")
 public class Estado {
- 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_estado;
- 
-    private String nombre_estado;
+    @Column(name = "id_estado")
+    private Long idEstado;
+
+    @Column(name = "nombre_estado", nullable = false, length = 100)
+    private String nombreEstado;
+
+    @OneToMany(mappedBy = "estado", cascade = CascadeType.ALL)
+    private List<Arbol> arboles;
 }
